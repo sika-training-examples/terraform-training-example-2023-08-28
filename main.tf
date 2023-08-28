5f866dff4609dc003dbcff78c4001cedaec43fd0
Ondrej Sika <ondrej@ondrejsika.com>
2023-08-28 11:40:29 +0200

    chore: Move some config to locals

--- main.tf
+++ main.tf
@@ -18,6 +18,12 @@ locals {
   }
 }
 
+locals {
+  aks_name       = "example"
+  aks_node_count = 3
+  aks_node_size  = "Standard_D2_v2"
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example"
   location = local.location
@@ -29,13 +35,13 @@ resource "azurerm_kubernetes_cluster" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
-  name       = "example"
-  dns_prefix = "example"
+  name       = local.aks_name
+  dns_prefix = local.aks_name
 
   default_node_pool {
     name       = "default"
-    node_count = 3
-    vm_size    = "Standard_D2_v2"
+    node_count = local.aks_node_count
+    vm_size    = local.aks_node_size
   }
 
   identity {
